b92a27d73e3a45b5fe4e575058598244933acee6
eguser <egjerasi@salesforce.com>
2024-07-11 20:14:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TestMe Extension
+
+Add description ...
+
+
+...
+
+
+...
+
+
+Add this dependency to your application pom.xml
+
+```
+<groupId>org.mule.extension</groupId>
+<artifactId>test-me</artifactId>
+<version>0.0.1</version>
+<classifier>mule-plugin</classifier>
+```
---
+++ src/main/java/org/mule/extension/testme/internal/TestMeExtension.java
@@ -0,0 +1,20 @@
+package org.mule.extension.testme.internal;
+
+import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Configurations;
+import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.sdk.api.annotation.JavaVersionSupport;
+import org.mule.sdk.api.meta.JavaVersion;
+
+
+/**
+ * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
+ * and sources are going to be declared.
+ */
+@Xml(prefix = "testme")
+@Extension(name = "TestMe")
+@Configurations(TestMeConfiguration.class)
+@JavaVersionSupport({JavaVersion.JAVA_8, JavaVersion.JAVA_11, JavaVersion.JAVA_17})
+public class TestMeExtension {
+
+}
