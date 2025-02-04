6de82b3c378dc9da8756e24ceb625ccff5fe95f3
eguser <egjerasi@salesforce.com>
2025-02-04 15:23:30 -0800

    Support Java 8,11,17

--- src/main/java/org/mule/pdf/extension/internal/PDFModuleExtension.java
+++ src/main/java/org/mule/pdf/extension/internal/PDFModuleExtension.java
@@ -1,7 +1,9 @@
 package org.mule.pdf.extension.internal;
 
 import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Operations;
 import org.mule.pdf.extension.internal.config.PDFModuleConfiguration;
+import org.mule.pdf.extension.internal.operation.SplitPDFOperation;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 import org.mule.sdk.api.annotation.JavaVersionSupport;
@@ -12,10 +14,11 @@ import org.mule.sdk.api.meta.JavaVersion;
  * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
  * and sources are going to be declared.
  */
-@Xml(prefix = "testme")
-@Extension(name = "TestMe")
+@Xml(prefix = "pdf")
+@Extension(name = "PDF")
 @Configurations(PDFModuleConfiguration.class)
-@JavaVersionSupport({JavaVersion.JAVA_11, JavaVersion.JAVA_17})
+@Operations({SplitPDFOperation.class})
+@JavaVersionSupport({JavaVersion.JAVA_8, JavaVersion.JAVA_11, JavaVersion.JAVA_17})
 public class PDFModuleExtension {
 
 }
