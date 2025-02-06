5028678f64ae64bf1abd325b70b586c3b26d28ee
eguser <egjerasi@salesforce.com>
2025-02-05 22:31:44 -0800

    Actually perform the Split and Save operation

--- src/main/java/org/mule/pdf/extension/api/PDFAttributes.java
+++ src/main/java/org/mule/pdf/extension/api/PDFAttributes.java
@@ -3,6 +3,7 @@ package org.mule.pdf.extension.api;
 public class PDFAttributes {
     private int pageCount;
     private int fileSize;
+    private String fileName;
 
     public int getPageCount() {
         return pageCount;
@@ -18,5 +19,13 @@ public class PDFAttributes {
 
     public void setFileSize(int fileSize) {
         this.fileSize = fileSize;
+    }
+    
+    public String getFileName() {
+    	return fileName;
+    }
+
+    public void setFileName(String newFileName) {
+        this.fileName = newFileName;
     }
 }
--- src/main/java/org/mule/pdf/extension/internal/operation/SplitPDFOperation.java
+++ src/main/java/org/mule/pdf/extension/internal/operation/SplitPDFOperation.java
@@ -1,5 +1,7 @@
 package org.mule.pdf.extension.internal.operation;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -7,65 +9,88 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.io.IOUtils;
 import org.apache.pdfbox.io.RandomAccessReadBuffer;
+import org.apache.pdfbox.multipdf.Splitter;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.mule.runtime.extension.api.annotation.param.MediaType;
 import org.mule.runtime.extension.api.annotation.param.Config;
 import org.mule.runtime.extension.api.annotation.param.Optional;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Example;
+import org.mule.runtime.extension.api.annotation.param.display.Summary;
 import org.mule.pdf.extension.api.PDFAttributes;
 import org.mule.pdf.extension.internal.config.PDFModuleConfiguration;
 import org.mule.runtime.extension.api.annotation.metadata.fixed.OutputJsonType;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.streaming.StreamingHelper;
+import org.slf4j.Logger;
 
 public class SplitPDFOperation {
+	private static final Logger LOGGER = getLogger(SplitPDFOperation.class);
 	
 	@MediaType(value = MediaType.APPLICATION_JSON)
     @OutputJsonType(schema = "metadata/PDFSplitterOperationOutput.json")
     @DisplayName("Split PDF")
     public List<Result<InputStream, PDFAttributes>> splitPdf(
     		@Config PDFModuleConfiguration config,
-            InputStream pdfPayload, 
+    		@DisplayName("Original File Name") @Summary("The file name of the PDF Document we will split.")
+    			String fileName,
+    		@DisplayName("Original PDF Payload")
+            	InputStream pdfPayload,
+            @DisplayName("Split Label") @Example("Part")
+            	String splitLabel,
+            String labelSeparator,
             int maxPages,
             StreamingHelper streamingHelper) throws Exception {
+
+		List<Result<InputStream, PDFAttributes>> pdfPartsList = new ArrayList<>();
+
+		Splitter splitter = new Splitter();
+		splitter.setSplitAtPage(maxPages);
 		
-		List<Result<InputStream, PDFAttributes>> resultList = new ArrayList<>();
-		
-		try (PDDocument document = Loader.loadPDF(new RandomAccessReadBuffer(pdfPayload))) {
-            int totalPages = document.getNumberOfPages();
+		try (PDDocument originalPdfDocument = Loader.loadPDF(new RandomAccessReadBuffer(pdfPayload))){
+
+            int totalPages = originalPdfDocument.getNumberOfPages();
             int splitCounter = 0;
             
-            // Loop through and split the document
-            for (int i = 0; i < totalPages; i += maxPages) {
-                PDDocument splitDoc = new PDDocument();
-                
-                for (int j = 0; j < maxPages && (i + j) < totalPages; j++) {
-                    splitDoc.addPage(document.getPage(i + j));
-                }
-
-                // Convert the split document to an InputStream
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                splitDoc.save(outputStream);
-                InputStream splitStream = new ByteArrayInputStream(outputStream.toByteArray());
-                
-                // Collect metadata
-                PDFAttributes attributes = new PDFAttributes();
-                attributes.setPageCount(splitDoc.getNumberOfPages());
-                attributes.setFileSize(outputStream.size());
-
-                resultList.add(Result.<InputStream, PDFAttributes>builder()
-                        .output(splitStream)
-                        .attributes(attributes)
-                        .build());
-                
-                splitDoc.close();
-                splitCounter++;
+            if (totalPages > maxPages) {
+            	List<PDDocument> originalPdfDocumentParts = splitter.split(originalPdfDocument);
+            	
+            	for (PDDocument pdfDocumentPart : originalPdfDocumentParts) {
+            		try (
+            				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();            			
+                			InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+            		) {
+            			pdfDocumentPart.save(outputStream);
+            			
+            			// Collect metadata
+                        PDFAttributes attributes = new PDFAttributes();
+                        attributes.setPageCount(pdfDocumentPart.getNumberOfPages());
+                        attributes.setFileSize(outputStream.size());
+                        attributes.setFileName(fileName + labelSeparator + splitLabel + splitCounter);
+                        
+            			pdfPartsList.add(Result.<InputStream, PDFAttributes>builder()
+            					.output(inputStream)
+            					.attributes(null)
+            					.build());
+            			
+            			pdfDocumentPart.close();
+            		} catch (Exception e) {
+						LOGGER.error("Exception " + e.getMessage() + " occurred. Closing the pdfDocumentPart.");
+						pdfDocumentPart.close();
+            		}
+            	}
+            } else {
+            	LOGGER.info("originalPdfDocument has fewer pages than " + maxPages);
+            	pdfPartsList.add(Result.<InputStream, PDFAttributes>builder()
+    					.output(pdfPayload)
+    					.attributes(null)
+    					.build());
             }
         }
 		
-		
-		return resultList;
+		return pdfPartsList;
 		
 	}
 
