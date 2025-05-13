22e41727fb8b6fb9d2e7c3e8aec110ed44ae07ac
eguser <egjerasi@salesforce.com>
2025-05-13 10:34:17 -0700

    Removing piped streams and schedulerService

--- src/main/java/org/mule/pdf/extension/internal/operation/HtmlToPdfOperation.java
+++ src/main/java/org/mule/pdf/extension/internal/operation/HtmlToPdfOperation.java
@@ -2,25 +2,21 @@ package org.mule.pdf.extension.internal.operation;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 import java.time.Instant;
-
-import javax.inject.Inject;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Entities;
 import org.mule.pdf.extension.api.PDFAttributes;
-import org.mule.runtime.api.scheduler.SchedulerService;
 import org.mule.runtime.extension.api.annotation.param.Content;
 import org.mule.runtime.extension.api.annotation.param.MediaType;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
 import org.mule.runtime.extension.api.annotation.param.display.Summary;
 import org.mule.runtime.extension.api.runtime.operation.Result;
 import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
-import org.mule.runtime.extension.api.runtime.streaming.StreamingHelper;
 import org.slf4j.Logger;
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
@@ -28,8 +24,8 @@ import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 public class HtmlToPdfOperation {
     private static final Logger LOGGER = getLogger(HtmlToPdfOperation.class);
 
-    @Inject
-    private SchedulerService schedulerService;
+    // @Inject
+    // private SchedulerService schedulerService;
 
     @MediaType(value = MediaType.ANY, strict = false)
     @DisplayName("HTML to PDF")
@@ -40,7 +36,6 @@ public class HtmlToPdfOperation {
                 InputStream htmlPayload,
             @DisplayName("Output File Name") @Summary("The file name of the PDF Document we will produce.")
                 String outputPdfFileName,
-            StreamingHelper streamingHelper,
             CompletionCallback<InputStream, PDFAttributes> callback) throws Exception {
 
         Long epochStart = Instant.now().toEpochMilli();
@@ -53,12 +48,12 @@ public class HtmlToPdfOperation {
                     .charset("UTF-8")
                     .escapeMode(Entities.EscapeMode.xhtml);
 
-        PipedOutputStream outputStream = new PipedOutputStream();
-        PipedInputStream responsePdfContent = new PipedInputStream(outputStream);
-        //ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        //InputStream responsePdfContent = new ByteArrayInputStream(outputStream.toByteArray());
+        //PipedOutputStream outputStream = new PipedOutputStream();
+        //PipedInputStream responsePdfContent = new PipedInputStream(outputStream);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        InputStream responsePdfContent = new ByteArrayInputStream(outputStream.toByteArray());
 
-        schedulerService.ioScheduler().submit(() -> {
+        //schedulerService.ioScheduler().submit(() -> {
             try ( outputStream ) {
                 PdfRendererBuilder builder = new PdfRendererBuilder();
                 builder.withHtmlContent(htmlDocument.selectFirst("html").outerHtml(), null);
@@ -67,7 +62,7 @@ public class HtmlToPdfOperation {
             } catch (Exception e) {
                 callback.error(e);
             }
-        });
+        //});
 
         PDFAttributes responsePdfAttributes = new PDFAttributes();
         responsePdfAttributes.setFileName(outputPdfFileName);
