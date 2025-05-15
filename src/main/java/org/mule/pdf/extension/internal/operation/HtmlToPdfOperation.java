a7e2d5c48359da7b30032dbf31305ebcd3bfba8e
eguser <egjerasi@salesforce.com>
2025-05-15 15:28:30 -0700

    Adding Metadata and Tests

--- src/main/java/org/mule/pdf/extension/internal/operation/HtmlToPdfOperation.java
+++ src/main/java/org/mule/pdf/extension/internal/operation/HtmlToPdfOperation.java
@@ -11,6 +11,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Entities;
 import org.mule.pdf.extension.api.PDFAttributes;
+import org.mule.pdf.extension.internal.metadata.BinaryMetadataResolver;
+import org.mule.runtime.extension.api.annotation.metadata.OutputResolver;
 import org.mule.runtime.extension.api.annotation.param.Content;
 import org.mule.runtime.extension.api.annotation.param.MediaType;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
@@ -20,20 +22,17 @@ import org.mule.runtime.extension.api.runtime.process.CompletionCallback;
 import org.slf4j.Logger;
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+import com.openhtmltopdf.util.XRLog;
 
 public class HtmlToPdfOperation {
     private static final Logger LOGGER = getLogger(HtmlToPdfOperation.class);
 
-    // @Inject
-    // private SchedulerService schedulerService;
-
-    @MediaType(value = MediaType.ANY, strict = false)
+    @MediaType("application/pdf")
+    @OutputResolver(output = BinaryMetadataResolver.class)
     @DisplayName("HTML to PDF")
     public void htmlToPdf(
-            //@Config PDFModuleConfiguration config,
             @DisplayName("HTML Payload")
-                @Content //@TypeResolver(BinaryMetadataResolver.class)
-                InputStream htmlPayload,
+                @Content InputStream htmlPayload,
             @DisplayName("Output File Name") @Summary("The file name of the PDF Document we will produce.")
                 String outputPdfFileName,
             CompletionCallback<InputStream, PDFAttributes> callback) throws Exception {
@@ -48,32 +47,29 @@ public class HtmlToPdfOperation {
                     .charset("UTF-8")
                     .escapeMode(Entities.EscapeMode.xhtml);
 
-        //PipedOutputStream outputStream = new PipedOutputStream();
-        //PipedInputStream responsePdfContent = new PipedInputStream(outputStream);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        InputStream responsePdfContent = new ByteArrayInputStream(outputStream.toByteArray());
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            //XRLog.setLevel(XRLog.GENERAL, Level.SEVERE); //setting it to avoid css warnings in log
+            XRLog.listRegisteredLoggers().forEach(logger -> XRLog.setLevel(logger, java.util.logging.Level.SEVERE));
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.withHtmlContent(htmlDocument.selectFirst("html").outerHtml(), null);
+            builder.toStream(outputStream);
+            builder.run();
 
-        //schedulerService.ioScheduler().submit(() -> {
-            try ( outputStream ) {
-                PdfRendererBuilder builder = new PdfRendererBuilder();
-                builder.withHtmlContent(htmlDocument.selectFirst("html").outerHtml(), null);
-                builder.toStream(outputStream);
-                builder.run();
-            } catch (Exception e) {
-                callback.error(e);
-            }
-        //});
+            PDFAttributes responsePdfAttributes = new PDFAttributes();
+            responsePdfAttributes.setFileName(outputPdfFileName);
 
-        PDFAttributes responsePdfAttributes = new PDFAttributes();
-        responsePdfAttributes.setFileName(outputPdfFileName);
+            InputStream responsePdfContent = new ByteArrayInputStream(outputStream.toByteArray());
 
-        LOGGER.info("HTML to PDF completed in " + (Instant.now().toEpochMilli() - epochStart));
-        System.out.println("HTML to PDF completed in " + (Instant.now().toEpochMilli() - epochStart));
+            LOGGER.info("HTML to PDF completed in " + (Instant.now().toEpochMilli() - epochStart) + "ms.");
 
-        callback.success(Result.<InputStream, PDFAttributes>builder()
-                .output(responsePdfContent)
-                .mediaType(org.mule.runtime.api.metadata.MediaType.parse("application/pdf"))
-                .attributes(responsePdfAttributes)
-                .build());
+            callback.success(Result.<InputStream, PDFAttributes>builder()
+                    .output(responsePdfContent)
+                    .mediaType(org.mule.runtime.api.metadata.MediaType.parse("application/pdf"))
+                    .attributes(responsePdfAttributes)
+                    .build());
+
+        } catch (Exception e) {
+            callback.error(e);
+        }
     }
 }
